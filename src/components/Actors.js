4b593cf59f70a1694c6ada9a63a5c84e7193c26c
Hurly77 <cameron.leverett@yahoo.com>
2020-10-16 18:42:53 -0700

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,19 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((actor, id)=>{
+        return(
+          <div key={id}>
+            <h2>{actor.name}</h2>
+            <ul>
+              {actor.movies.map((movie, id)=>{
+               return( <li key={id}>{movie}</li>)
+              })}
+            </ul>
+          </div>
+        )
+      })}
     </div>
   );
 };
